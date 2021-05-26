545b2b8a18275b81472c1bd02d107e9d77e021f6
zuri03 <zls6563@g.rit.edu>
2021-05-26 15:25:54 -0400

    Initial set up of moving pieces

--- src/Checkers.js
+++ src/Checkers.js
@@ -5,6 +5,7 @@ class Space extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            id: this.props.id,
             currentPiece : this.props.piece,
             spaceColor : this.props.spaceColor,
             index : this.props.index
@@ -20,46 +21,85 @@ class Space extends React.Component {
 
     render(){
         return (
-            <div className={this.state.spaceColor}>{this.state.currentPiece}</div>   
+            <button id={this.state.id} className={this.state.spaceColor} onClick={(id) => this.props.handleClick(this.state.id)}>
+                {this.state.currentPiece}
+            </button>   
         );
     }
 }
 
 function Piece(props){
     return (
-       <button className="Checkers-piece"></button>
+       <button className={props.pieceColor} ondragStart={props.drag} draggable="true"></button>
     );
 }
 class Board extends React.Component {
 
     constructor(props){
+
         super(props);
+
         const rows = Array(8).fill(null);
         this.state = {
-            spaces : Array(8).fill(null).map(() => rows.slice())
+            spaces : Array(8).fill(null).map(() => rows.slice()),
+            start: null,//first click
+            end: null,//second click
         };
+
+        this.drag = this.drag.bind(this);
         
         var color = 0;
+        var pieceIndex = 0;
+        var spaceIndex = 0;
         for(var i = 0; i < this.state.spaces.length; i++){
 
             color = (i % 2) === 0 ? 1 : 0;
 
             for(var j = 0; j < this.state.spaces.length; j++){
                 if(color === 0){
-                    this.state.spaces[i][j] = <Space spaceColor="Checkers-whiteSpace" index={i + ", " + j}/>
+                    this.state.spaces[i][j] = <Space id={spaceIndex} spaceColor="Checkers-whiteSpace"/>
                     color++;
                 } else {
                     if(i <= 2){
-                        this.state.spaces[i][j] = <Space spaceColor="Checkers-blackSpace" index={i + ", " + j} piece={<Piece/>}/>
+                        this.state.spaces[i][j] = <Space id={spaceIndex} spaceColor="Checkers-blackSpace" handleClick={(id) => this.handleClick(id)}
+                            piece={<Piece id={pieceIndex} pieceColor="Checkers-pieceRed"/>}/>
+                        pieceIndex++;
                     } else if (i >= 5) {
-                        this.state.spaces[i][j] = <Space spaceColor="Checkers-blackSpace" index={i + ", " + j} piece={<Piece/>}/>
+                        this.state.spaces[i][j] = <Space id={spaceIndex} spaceColor="Checkers-blackSpace" handleClick={(id) => this.handleClick(id)}
+                            piece={<Piece id={pieceIndex} pieceColor="Checkers-pieceWhite"/>}/>
+                        pieceIndex++;
                     } else {
-                        this.state.spaces[i][j] = <Space spaceColor="Checkers-blackSpace" index={i + ", " + j} piece={null}/>
+                        this.state.spaces[i][j] = <Space id={spaceIndex} spaceColor="Checkers-blackSpace" handleClick={(id) => this.handleClick(id)} 
+                            piece={null}/>
                     }
                     color--;
                 }
+                spaceIndex++;
             }
         }
+    }
+
+    handleClick(id){
+        console.log('Space id: ' + id);
+        
+        if(this.state.start === null){
+            this.setState({start: id})
+        } else {
+            this.setState({end: id})
+            //this.moveMade();
+        }
+        
+    }
+
+    moveMade(){
+        
+    }
+    drag(e) {
+        e.dataTransfer.setData("Piece", e.target.id);
+        console.log(e.target.id);
+    }
+
+    movePiece(){
 
     }
 
@@ -90,6 +130,7 @@ class Board extends React.Component {
                 <div className="Checkers-row">
                     {this.state.spaces[7]}
                 </div>
+                <footer>start = {this.state.start} + {this.state.end}</footer>
             </div>
         );
     }
