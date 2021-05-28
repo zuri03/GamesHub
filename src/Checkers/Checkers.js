4f149a9ca037511f76aaf04ee2f09396bea329b1
zuri03 <zls6563@g.rit.edu>
2021-05-28 18:44:12 -0400

    Added jump moves and deslecting a piece

--- src/Checkers/Checkers.js
+++ src/Checkers/Checkers.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import validateMove from './Referee.js';
+import isValidMove from './Referee.js';
 
 function Space(props) {
     return (
-        <div id={props.id} className={props.spaceColor} onClick={(id, piece) => props.handleClick(props.id, props.piece)}> 
+        <div 
+            id={props.id} 
+            className={props.spaceColor} 
+            onClick={(id, piece) => props.handleClick(props.id, props.piece)}
+            onDoubleClick={props.handleDoubleClick}> 
             {props.piece}
         </div>   
     );
@@ -29,6 +33,8 @@ class Board extends React.Component {
             message: null,//lets the player know if a move was successful or if they have made a mistake
             dictionary: {},
         };
+
+        this.handleDoubleClick = this.handleDoubleClick.bind(this);
         
         var color = 0;
         var pieceIndex = 0;
@@ -59,7 +65,8 @@ class Board extends React.Component {
                         this.state.spaces[i][j] = <Space 
                             id={i + "," + j}  
                             spaceColor="Checkers-blackSpace" 
-                            handleClick={(id, piece) => this.handleClick(id, piece)} 
+                            handleClick={(id, piece) => this.handleClick(id, piece)}
+                            handleDoubleClick={this.handleDoubleClick} 
                             piece={this.state.dictionary[i + "," + j]}
                         />;
                         pieceIndex++;
@@ -73,6 +80,7 @@ class Board extends React.Component {
                             id={i + "," + j} 
                             spaceColor="Checkers-blackSpace" 
                             handleClick={(id, piece) => this.handleClick(id, piece)}
+                            handleDoubleClick={() => this.handleDoubleClick()}
                             piece={this.state.dictionary[i + "," + j]}
                         />;
                         pieceIndex++;
@@ -84,6 +92,7 @@ class Board extends React.Component {
                             id={i + "," + j}  
                             spaceColor="Checkers-blackSpace" 
                             handleClick={(id, piece) => this.handleClick(id, piece)} 
+                            handleDoubleClick={() => this.handleDoubleClick()}
                             piece={null}
                         />;   
                     }
@@ -96,42 +105,65 @@ class Board extends React.Component {
     async handleClick(id, piece){
         
         if(this.state.start === null){
-            if(piece === null){
-                this.setState({message: 'this space does not have a piece'});
-            } else {
-                await this.setState({
-                    start: id,
-                    message: null
-                });
-            }
+
+            piece === null ? //condition
+                this.setState({message: 'this space does not have a piece'}) ://true 
+                await this.setState({start: id, message: null});//false
+            
         } else {
+
             //since set state is asynchronous I ran into issues with updating the end property, the await statement fixed that
             if(this.state.dictionary[id] === null){
                 
-                console.log('Before Validate:\n ' + 'start: ' + this.state.start + '\n end: ' + this.state.end);
-                await this.setState({
-                    end: id,
-                    message: null
-                });
-                if(validateMove(this.state.start, this.state.end)){
-                    this.movePiece();
-                } else {
-                    this.setState({message: 'Illegal Move'})
-                }
+                await this.setState({end: id, message: null});
+
+                isValidMove(this.state.start, this.state.end, this.getDiagonalPiece()) ?//condition
+                    this.movePiece() ://true
+                    this.setState({message: 'Illegal Move'});//false
+        
             } else {
                 this.setState({message: 'Cannot place piece here this space is full'});
             }
         }   
     }
+
+    handleDoubleClick() {
+        this.setState({
+            start: null,
+            end: null,
+            message: null
+        });
+    }
+
+    //gotta fix
+    getDiagonalPiece(){
+        var gapIndex = [];
+        
+        console.log('Coordinates:  \n ' + 'Start: ' + this.state.start + '\n End: ' + this.state.end);
+        this.state.start.split(',')[0] > this.state.end.split(',')[0] ? //condition 
+            gapIndex.push((parseInt(this.state.start.split(',')[0]) - 1).toString()) ://true
+            gapIndex.push((parseInt(this.state.start.split(',')[0]) + 1).toString());//false
+        
+        this.state.start.split(',')[1] > this.state.end.split(',')[1] ? //condition 
+            gapIndex.push((parseInt(this.state.start.split(',')[1]) - 1).toString()) ://true
+            gapIndex.push((parseInt(this.state.start.split(',')[1]) + 1).toString());//false
+        
+        console.log('Array: ' + gapIndex);
+        console.log('Diag Indexes: ' + gapIndex.join());
+        //console.log('Returned Pice: ' + this.state.dictionary[gapIndex.join()])
+        return this.state.dictionary[gapIndex.join()];
+    }
     
     movePiece(){
-        //When attempting to move the same piece twice I get the screenshotted error
+
+        //Copy all of the necessary state properties 
         var startIndex = this.state.start.split(',');
         var endIndex = this.state.end.split(',');
         var movingPiece = this.state.dictionary[startIndex];
         var udpatedSpace = this.state.spaces;
         var updatedDiction = this.state.dictionary;
 
+        //replace the destination space with a piece
         udpatedSpace[endIndex[0]][endIndex[1]] = null
         udpatedSpace[endIndex[0]][endIndex[1]] = <Space 
             id={this.state.end} 
@@ -140,6 +172,7 @@ class Board extends React.Component {
             piece={this.state.dictionary[this.state.start.split(',')]}
         />;
 
+        //replace the original space with a null piece
         updatedDiction[this.state.end] = this.state.dictionary[this.state.start];
         udpatedSpace[startIndex[0]][startIndex[1]] = null
         udpatedSpace[startIndex[0]][startIndex[1]] = <Space 
@@ -149,9 +182,11 @@ class Board extends React.Component {
             piece={null}
         />;
 
+        //update the state of the dictionary
         updatedDiction[this.state.start] = null;
         updatedDiction[this.state.end] = movingPiece;
         
+        //update component state
         this.setState({
             spaces: udpatedSpace,
             dictionary: updatedDiction,
@@ -161,32 +196,16 @@ class Board extends React.Component {
     }
 
     render() {
+        let index = 0;
+        let row;
+        let board = this.state.spaces.map(() => {
+            row = <div className="Checkers-row">{this.state.spaces[index]}</div>;
+            index++;
+            return row;
+        })
         return (
             <div className="Checkers-board">
-                <div className="Checkers-row">
-                    {this.state.spaces[0]}
-                </div>
-                <div className="Checkers-row">
-                    {this.state.spaces[1]}
-                </div>
-                <div className="Checkers-row">
-                    {this.state.spaces[2]}
-                </div>
-                <div className="Checkers-row">
-                    {this.state.spaces[3]}
-                </div>
-                <div className="Checkers-row">
-                    {this.state.spaces[4]}
-                </div>
-                <div className="Checkers-row">
-                    {this.state.spaces[5]}
-                </div>
-                <div className="Checkers-row">
-                    {this.state.spaces[6]}
-                </div>
-                <div className="Checkers-row">
-                    {this.state.spaces[7]}
-                </div>
+                {board}
                 <footer>{this.state.start} + {this.state.message} + {this.state.end}</footer>
             </div>
         );
--- src/Checkers/Referee.js
+++ src/Checkers/Referee.js
@@ -1,18 +1,16 @@
 //start and end are the id of the start and destination of a piece Ex: start = '0,7'
-const validateMove = (start, end) => {
+const isValidMove = (start, end, diagPiece) => {
+
+    console.log('Diag: ' + diagPiece);
     var endIndex = end.split(',');
     var startIndex = start.split(',');
-
-    //Because of the checkered pattern on the board this will determine of the endspace is a white space 
-    if((endIndex[0] % 2 === 0 && endIndex[1] % 2 === 0) || (endIndex[0] % 2 !== 0 && endIndex[1] % 2 !== 0)){
+    
+    return diagPiece === null ?
+        ((endIndex[0] % 2 === 0 && endIndex[1] % 2 === 0) || (endIndex[0] % 2 !== 0 && endIndex[1] % 2 !== 0)) &&  
+            (Math.abs(startIndex[0] - endIndex[0]) === 1 && Math.abs(startIndex[1] - endIndex[1]) === 1) :
+        ((Math.abs(endIndex[0] - startIndex[0]) === 2) && (Math.abs(endIndex[1] - startIndex[1])) === 2)
+    
             
-        //Make sure player only moves vertically or horizontally one space
-        if(Math.abs(startIndex[0] - endIndex[0]) === 1 && Math.abs(startIndex[1] - endIndex[1]) === 1){
-            return true;
-        }
-        return false;     
-    } 
-    return false;    
 }
 
-export default validateMove;
+export default isValidMove;
